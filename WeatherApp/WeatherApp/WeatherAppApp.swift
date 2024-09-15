ec037c78b745f32d3c14f96b61ccf43315193ed9
brainfck <mykhailo.tsuber@gmail.com>
2024-09-15 22:07:54 +0200

    initial commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 9/15/24.
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
