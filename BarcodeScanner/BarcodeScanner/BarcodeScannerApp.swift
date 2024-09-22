379466cd4b2b0b4988636a1ef6801113eac1f6dd
brainfck <mykhailo.tsuber@gmail.com>
2024-09-22 15:52:43 +0200

    initial commit

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Mykhailo Tsuber on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
