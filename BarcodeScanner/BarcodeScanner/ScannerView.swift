a34390e239377daac0d6acf176ff90d192e4b917
brainfck <mykhailo.tsuber@gmail.com>
2024-09-26 20:36:11 +0200

    Coordinator Setup

---
+++ BarcodeScanner/BarcodeScanner/ScannerView.swift
@@ -0,0 +1,34 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Mykhailo Tsuber on 9/26/24.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+  func makeUIViewController(context: Context) -> ScannerVC {
+    ScannerVC(scannerDelegate: context.coordinator)
+  }
+
+  func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator()
+  }
+
+  final class Coordinator: NSObject, ScannerVCDelegate {
+    func didFind(barcode: String) {
+      print(barcode)
+    }
+
+    func didSurface(error: CameraError) {
+      print(error.rawValue)
+    }
+  }
+}
+
+#Preview {
+  ScannerView()
+}
